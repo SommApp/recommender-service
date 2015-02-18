9a2db68b58b1e3413c6c84ff8e4ff884a66fa293
Seth <sjw3kf@mail.missouri.edu>
2015-02-17 20:16:41 -0600

    Finished VisitHandlerTest

--- src/test/java/com/capstone/recommender/controllers/VisitHandlerTest.java
+++ src/test/java/com/capstone/recommender/controllers/VisitHandlerTest.java
@@ -1,13 +1,10 @@
 package com.capstone.recommender.controllers;
 
 import com.capstone.recommender.models.PartialVisit;
-import com.google.common.cache.Cache;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import com.google.common.cache.CacheBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -36,18 +33,44 @@ public class VisitHandlerTest {
     }
 
     @Test
-    public void insertVisit() {
+    public void startVisit() {
         final long uid = 1;
         final long rid = 2;
 
         final long token = visitHandler.beginVisit(uid, rid);
 
-        assertEquals("Token is the wrong value", 1, token);
+        assertEquals("Token is the wrong value", 0, token);
         assertEquals("There are no users in the cache when there should be one", 1, cache.size());
 
-        PartialVisit visit = cache.get(token);
+        final PartialVisit visit = cache.get(token);
         assertNotNull("The user was not put in the cache", visit);
         assertEquals("Uid was changed when it should have remained the same", uid, visit.getUserId());
         assertEquals("Rid was changed when it should have remained the same", rid, visit.getRestaurantId());
+    }
+
+    @Test
+    public void endVisit() {
+        final long uid = 1;
+        final long rid = 2;
+
+        final long token = visitHandler.beginVisit(uid, rid);
+
+        visitHandler.endVisit(token);
+
+        final PartialVisit visit = cache.get(token);
+        assertNull("Restaurant was not removed when it should have been", visit);
+    }
+
+    @Test
+    public void badEndVisit() {
+        final long uid = 1;
+        final long rid = 2;
+
+        final long token = visitHandler.beginVisit(uid, rid);
+
+        visitHandler.endVisit(token+1);
+
+        final PartialVisit visit = cache.get(token);
+        assertNotNull("Restaurant was removed when it should not have been", visit);
     }
 }
