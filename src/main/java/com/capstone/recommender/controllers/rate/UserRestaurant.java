a879259cc8dedcb9657da2c441e9f04dd9ca8881
Seth <sjw3kf@mail.missouri.edu>
2015-02-23 16:23:07 -0600

    Created UserRestaurant pair to act as key in mapreduce

--- src/main/java/com/capstone/recommender/controllers/rate/UserRestaurant.java
+++ src/main/java/com/capstone/recommender/controllers/rate/UserRestaurant.java
@@ -1,7 +1,29 @@
 package com.capstone.recommender.controllers.rate;
 
+import javax.validation.constraints.NotNull;
+
 /**
- * Created by sethwiesman on 2/23/15.
+ * @author by sethwiesman on 2/23/15.
  */
-public class UserRestaurant {
+public class UserRestaurant implements Comparable<UserRestaurant> {
+
+    public final long uid;
+    public final long rid;
+
+    public UserRestaurant(long uid, long rid) {
+        this.uid = uid;
+        this.rid = rid;
+    }
+
+    @Override
+    public int compareTo(@NotNull UserRestaurant that) {
+
+        final long uidDifference = this.uid - that.uid;
+
+        if (uidDifference != 0) {
+            return (int)uidDifference;
+        }
+
+        return (int)(this.rid - that.uid);
+    }
 }
