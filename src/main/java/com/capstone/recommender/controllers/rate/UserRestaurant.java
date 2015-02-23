ecf89dad815aebc0f300f1f159b1c05d48a69be4
Seth <sjw3kf@mail.missouri.edu>
2015-02-23 16:40:24 -0600

    Implemented Writable interfaces to UserRestaurant

--- src/main/java/com/capstone/recommender/controllers/rate/UserRestaurant.java
+++ src/main/java/com/capstone/recommender/controllers/rate/UserRestaurant.java
@@ -1,14 +1,20 @@
 package com.capstone.recommender.controllers.rate;
 
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+
 import javax.validation.constraints.NotNull;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
 /**
  * @author by sethwiesman on 2/23/15.
  */
-public class UserRestaurant implements Comparable<UserRestaurant> {
+public class UserRestaurant implements Comparable<UserRestaurant>, Writable, WritableComparable<UserRestaurant>{
 
-    public final long uid;
-    public final long rid;
+    private long uid;
+    private long rid;
 
     public UserRestaurant(long uid, long rid) {
         this.uid = uid;
@@ -25,5 +31,36 @@ public class UserRestaurant implements Comparable<UserRestaurant> {
         }
 
         return (int)(this.rid - that.uid);
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public long getRid() {
+        return rid;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeLong(uid);
+        dataOutput.writeLong(uid);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.uid = dataInput.readLong();
+        this.rid = dataInput.readLong();
+    }
+
+    @Override
+    //hashing method described by Joshua Bloc in Effective Java
+    public int hashCode() {
+        final int shift = 32;
+        final int prime = 31;
+
+        int result = (int) (uid ^ (uid >>> shift));
+        result = prime * result + (int) (uid ^ (uid >>> shift));
+        return result;
     }
 }
