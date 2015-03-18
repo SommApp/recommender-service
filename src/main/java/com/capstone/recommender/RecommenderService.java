b8618abbac1a82ca627d11008c4f7c08a521907a
Seth <sjw3kf@mail.missouri.edu>
2015-03-18 16:52:19 -0500

    removed unnecessary parts

--- src/main/java/com/capstone/recommender/RecommenderService.java
+++ src/main/java/com/capstone/recommender/RecommenderService.java
@@ -1,19 +1,12 @@
 package com.capstone.recommender;
 
-import com.capstone.recommender.health.TemplateHealthCheck;
-import com.capstone.recommender.resources.RecommenderResource;
-import com.yammer.dropwizard.Service;
-import com.yammer.dropwizard.config.Environment;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
-public class RecommenderService extends Service<RecommenderConfiguration> {
+public class RecommenderService {
 	public static void main(String[] args) throws Exception {
-		//new RecommenderService().run(args);
-
         Configuration conf = new Configuration();
         conf.addResource(new Path("/usr/local/hadoop-1.2.1/conf/core-site.xml"));
         conf.addResource(new Path("/usr/local/hadoop-1.2.1/conf/hdfs-site.xml"));
@@ -22,17 +15,5 @@ public class RecommenderService extends Service<RecommenderConfiguration> {
         out.writeUTF("Append demo...");
         fs.close();
 
-    }
-
-	private RecommenderService() {
-		super("recommender-service");
-	}
-
-	@Override
-	protected void initialize(RecommenderConfiguration configuration, Environment environment) {
-		final String template = configuration.getTemplate();
-		final String defaultName = configuration.getDefaultName();
-		environment.addResource(new RecommenderResource(template, defaultName));
-		environment.addHealthCheck(new TemplateHealthCheck(template));
     }
 }
--- src/main/java/com/capstone/recommender/models/Analytic.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.recommender.models;
-
-/**
- * Created by sethwiesman on 2/5/15.
- */
-public class Analytic {
-}
