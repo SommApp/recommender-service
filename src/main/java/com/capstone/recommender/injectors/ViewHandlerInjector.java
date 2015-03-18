88ae8b8d7337ccd36d8f0bf1017efe339ff8f3f3
Seth <sjw3kf@mail.missouri.edu>
2015-03-17 22:00:04 -0500

    updated guice injector

--- src/main/java/com/capstone/recommender/injectors/ViewHandlerInjector.java
+++ src/main/java/com/capstone/recommender/injectors/ViewHandlerInjector.java
@@ -1,16 +1,8 @@
 package com.capstone.recommender.injectors;
 
-import com.google.common.cache.CacheBuilder;
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.Configuration;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,23 +14,6 @@ public class ViewHandlerInjector extends AbstractModule {
     @Override
     protected void configure() {
         bind(Map.class).to(ConcurrentHashMap.class);
-        bind(OutputStream.class).to(OutputStream.class);
-    }
-
-    @Provides
-    OutputStream provideOutputStream() {
-        final String dest = "/usr/restaurant/visits";
-        final Configuration conf = new Configuration();
-        try {
-            final FileSystem fs = FileSystem.get(URI.create(dest), conf);
-
-            return fs.create(new Path(dest), () -> {
-                System.out.print("*");
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+        bind(Collection.class).to(ArrayList.class);
     }
 }
