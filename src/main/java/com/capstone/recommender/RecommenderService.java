d9b8519a0ec2a20b1e8bae26225de4b808ba0230
Seth <sjw3kf@mail.missouri.edu>
2015-03-18 16:45:44 -0500

    test

--- src/main/java/com/capstone/recommender/RecommenderService.java
+++ src/main/java/com/capstone/recommender/RecommenderService.java
@@ -5,10 +5,24 @@ import com.capstone.recommender.resources.RecommenderResource;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Environment;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
 public class RecommenderService extends Service<RecommenderConfiguration> {
 	public static void main(String[] args) throws Exception {
-		new RecommenderService().run(args);
-	}
+		//new RecommenderService().run(args);
+
+        Configuration conf = new Configuration();
+        conf.addResource(new Path("/usr/local/hadoop-1.2.1/conf/core-site.xml"));
+        conf.addResource(new Path("/usr/local/hadoop-1.2.1/conf/hdfs-site.xml"));
+        FileSystem fs = FileSystem.get(conf);
+        FSDataOutputStream out = fs.append(new Path("/demo.txt"));
+        out.writeUTF("Append demo...");
+        fs.close();
+
+    }
 
 	private RecommenderService() {
 		super("recommender-service");
