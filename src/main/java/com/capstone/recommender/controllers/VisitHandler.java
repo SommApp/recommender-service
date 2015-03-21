8f3e8494a0080e11b7146a1aaee39838c206dd55
Seth <sjw3kf@mail.missouri.edu>
2015-03-21 14:21:30 -0500

    write ot file

--- src/main/java/com/capstone/recommender/controllers/VisitHandler.java
+++ src/main/java/com/capstone/recommender/controllers/VisitHandler.java
@@ -7,21 +7,31 @@ package com.capstone.recommender.controllers;
 import com.capstone.recommender.models.CompleteVisit;
 import com.capstone.recommender.models.PartialVisit;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class VisitHandler {
+public class VisitHandler implements Runnable{
 
     private final Map<Long, PartialVisit> visitByToken;
     private final AtomicLong tokenGenerator;
+    private final AtomicLong fileTokenGenerator;
     private final ArrayList<CompleteVisit> completeVisits;
+    private final ScheduledExecutorService executorService;
 
     public VisitHandler() {
         this.visitByToken = new ConcurrentHashMap<>();
         this.tokenGenerator = new AtomicLong();
+        this.fileTokenGenerator = new AtomicLong();
         this.completeVisits = new ArrayList<>();
+        this.executorService = new ScheduledThreadPoolExecutor(1);
     }
 
     public long beginVisit(long uid, long rid) {
@@ -40,5 +50,26 @@ public class VisitHandler {
         final CompleteVisit completeVisit = new CompleteVisit(visit);
         this.completeVisits.add(completeVisit);
         return true;
+    }
+
+    @Override
+    public void run() {
+        final long token = fileTokenGenerator.getAndIncrement();
+        FileOutputStream out = null;
+
+        try {
+            out = new FileOutputStream("/user/rest/visit/" + token);
+            final PrintStream stream = new PrintStream(out);
+            completeVisits.stream().forEach(stream::println);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null)
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
     }
 }
