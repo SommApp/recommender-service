7a27f16e79aee8712c3823f66df7cc18861f5a41
Seth <sjw3kf@mail.missouri.edu>
2015-03-21 15:12:20 -0500

    db

--- src/main/java/com/capstone/recommender/controllers/VisitHandler.java
+++ src/main/java/com/capstone/recommender/controllers/VisitHandler.java
@@ -7,30 +7,40 @@ package com.capstone.recommender.controllers;
 import com.capstone.recommender.models.CompleteVisit;
 import com.capstone.recommender.models.PartialVisit;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.sql.*;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class VisitHandler implements Runnable{
+public class VisitHandler {
 
     private final Map<Long, PartialVisit> visitByToken;
     private final AtomicLong tokenGenerator;
-    private final AtomicLong fileTokenGenerator;
-    private final ArrayList<CompleteVisit> completeVisits;
-    private final ScheduledExecutorService executorService;
+    private Connection connection;
+    private PreparedStatement preparedStatement;
 
     public VisitHandler() {
         this.visitByToken = new ConcurrentHashMap<>();
         this.tokenGenerator = new AtomicLong();
-        this.fileTokenGenerator = new AtomicLong();
-        this.completeVisits = new ArrayList<>();
-        this.executorService = new ScheduledThreadPoolExecutor(1);
-        this.executorService.scheduleAtFixedRate(this, 1, 1, TimeUnit.MINUTES);
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost/feedback?user=root&password=toor");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            this.preparedStatement = connection.prepareStatement("INSERT INTO Visits (uid, rid, date, duration) VALUES (?, ?, ?, ?)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public long beginVisit(long uid, long rid) {
@@ -40,38 +50,19 @@ public class VisitHandler implements Runnable{
         return token;
     }
 
-    public boolean endVisit(long token) {
+    public boolean endVisit(long token) throws SQLException {
         final PartialVisit visit = this.visitByToken.remove(token);
         if (visit == null) {
             return false;
         }
 
         final CompleteVisit completeVisit = new CompleteVisit(visit);
-        this.completeVisits.add(completeVisit);
+        preparedStatement.setString(1, String.valueOf(completeVisit.getUserId()));
+        preparedStatement.setString(2, String.valueOf(completeVisit.getRestaurantId()));
+        preparedStatement.setString(3, String.valueOf(completeVisit.getBeginVisit()));
+        preparedStatement.setString(4, String.valueOf(completeVisit.getDurationInMilliseconds()));
+
+        preparedStatement.executeQuery();
         return true;
-    }
-
-    @Override
-    public void run() {
-        final long token = fileTokenGenerator.getAndIncrement();
-        File out = null;
-
-        try {
-            out = new File("/user/rest/visit/" + token + ".txt");
-            if (!out.exists())
-                out.createNewFile();
-            final BufferedWriter stream = new BufferedWriter(new FileWriter(out));
-            completeVisits.stream().map(CompleteVisit::toString).forEach((e) -> {
-                try {
-                    stream.write(e);
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-            });
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
