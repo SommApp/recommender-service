6f275503fb8421ac058464c0c20bdab2504a182b
Seth <sjw3kf@mail.missouri.edu>
2015-03-27 17:21:29 -0500

    visit handler injector

---
+++ src/main/java/com/capstone/recommender/injectors/VisitHandlerInjector.java
@@ -0,0 +1,17 @@
+package com.capstone.recommender.injectors;
+
+import com.google.inject.AbstractModule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  @author sethwiesman on 3/27/15.
+ */
+public class VisitHandlerInjector extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(Map.class).to(HashMap.class);
+    }
+}
